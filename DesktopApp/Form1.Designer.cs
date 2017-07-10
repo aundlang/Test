109748c4f720de84445571c1907014d696c69eb3
DESKTOP-HBSQFNK\Ellerick <ellerick.esquivel@gmail.com>
2017-07-10 08:42:17 -0500

    add login form

--- DesktopApp/Form1.Designer.cs
+++ DesktopApp/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DesktopApp
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,12 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tb_User = new System.Windows.Forms.TextBox();
+            this.lb_User = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb_User
+            // 
+            this.tb_User.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_User.Location = new System.Drawing.Point(98, 212);
+            this.tb_User.Name = "tb_User";
+            this.tb_User.Size = new System.Drawing.Size(238, 40);
+            this.tb_User.TabIndex = 0;
+            // 
+            // lb_User
+            // 
+            this.lb_User.AutoSize = true;
+            this.lb_User.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_User.Location = new System.Drawing.Point(149, 153);
+            this.lb_User.Name = "lb_User";
+            this.lb_User.Size = new System.Drawing.Size(129, 32);
+            this.lb_User.TabIndex = 1;
+            this.lb_User.Text = "Usuario:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(98, 343);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(238, 40);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(127, 288);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Contraseña:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(178, 428);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(448, 585);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lb_User);
+            this.Controls.Add(this.tb_User);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tb_User;
+        private System.Windows.Forms.Label lb_User;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Platform.Parts/DAL/Connection.cs
+++ Platform.Parts/DAL/Connection.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
 
 namespace Platform.Parts.DAL
 {
     abstract class Connection
     {
+        private SqlConnection _sqlConnetion;
+
         public int Insert()
         {
             return 0;
